5dd06d80ef3a6df7991b2c1fc8fee50408686137
jgarta-codesource <jgartatests@gmail.com>
2025-10-27 10:12:14 -0600

    Update main.sh

--- Script/main.sh
+++ Script/main.sh
@@ -3,16 +3,16 @@
 # Step 1: Install required dependencies
 sudo su
 
-echo "==================================="
-echo "      Instaling dependencies       "
-echo "==================================="
+#echo "==================================="
+#echo "      Instaling dependencies       "
+#echo "==================================="
 sleep 2
 yum -y install nginx python3 python3-pip & wait
 
 
-echo "==================================="
-echo "      Creating project files       "
-echo "==================================="
+#echo "==================================="
+#echo "      Creating project files       "
+#echo "==================================="
 sleep 2
 
 # Step 2: Create project folders path
@@ -24,17 +24,17 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 # Step 4: Installing Gunicorn and Flask
-echo "==================================="
-echo "  Installing Gunicorn and Flask    "
-echo "==================================="
+#echo "==================================="
+#echo "  Installing Gunicorn and Flask    "
+#echo "==================================="
 sleep 2
 pip install --upgrade pip
 pip3 install gunicorn flask & wait
 
 
-echo "==================================="
-echo "       gninx configuration         "
-echo "==================================="
+#echo "==================================="
+#echo "       gninx configuration         "
+#echo "==================================="
 
 # Step 5: Create an endpoint
 curl https://raw.githubusercontent.com/jgarta-codesource/cloudflare-free-tier-demo/refs/heads/main/Endpoint%20server%20files/app.py -o app.py
@@ -44,9 +44,9 @@ curl https://raw.githubusercontent.com/jgarta-codesource/cloudflare-free-tier-de
 mkdir -p /etc/ssl/mycert/
 chmod 700 /etc/ssl/mycert/
 
-echo "==================================="
-echo " Type your domain configuration    "
-echo "==================================="
+#echo "==================================="
+#echo " Type your domain configuration    "
+#echo "==================================="
 
 sleep 3
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/mycert/selfsigned.key -out /etc/ssl/certs/selfsigned.crt -f
@@ -58,23 +58,4 @@ curl https://raw.githubusercontent.com/jgarta-codesource/cloudflare-free-tier-de
 
 setsebool -P httpd_can_network_connect 1
 
-
-### commands
-1. sudo su
-
-2. vi main.sh
-1. chmod +x main.sh
-2. ./main.sh
-
-
-vi nginx.conf
-cd /var/www/flask
-source .venv/bin/activate
-systemctl start nginx
-gunicorn -b 127.0.0.1:8080 app:app
-
-
-troubleshoot 
-cd /var/www/flask
-cd /etc/nginx
 
